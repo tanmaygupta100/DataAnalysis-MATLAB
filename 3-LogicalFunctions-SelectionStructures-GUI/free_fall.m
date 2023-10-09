6115cd0720425877ad9b2ba86132134aeaa03426
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2023-10-09 16:52:44 -0700

    Delete 3-LogicalFunctions-SelectionStructures-GUI/free_fall.m

--- 3-LogicalFunctions-SelectionStructures-GUI/free_fall.m
+++
@@ -1,9 +0,0 @@
-function [x,v,g]=free_fall(t)
-g=9.8;
-% g= gravity constant
-x=(1/2)*g*(t.^2)
-% x= distance
-v=g*t
-% v= velocity equation in meters/seconds
-a=g
-%a= acceleration in meters/seconds^2
